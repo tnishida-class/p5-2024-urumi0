44a511fe5b5705dc29693297da39c7e9af7a2faf
urumi0 <urumi1207@icloud.com>
2024-10-22 11:13:33 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,6 +11,16 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  
+  //blank
+  if (keyIsPressed) {
+    increment = 2;
+  } else {
+   increment = 1;
+    }
+    if (count < cycle/2) {
+   size = count + 50;
+   } else {
+   size = (cycle - count) + 50;
+   }
   ellipse(width / 2, height / 2, size);
 }
