22ca4ec0725160df00ab5bec4faf5107fea41953
urumi0 <urumi1207@icloud.com>
2024-10-22 11:25:09 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,18 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let increment;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  increment = 1;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + increment) % cycle;
   //blank
   if (keyIsPressed) {
     increment = 2;
